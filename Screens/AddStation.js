8236c73b6dcf30b067fdc8ea8b0ca256993de7ce
marvinkraus <marvin.kraus2310@outlook.de>
2022-11-08 16:54:31 +0100

    changes on addstation.js

--- Screens/AddStation.js
+++ Screens/AddStation.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
-
+import {Button} from '@react-native-material/core';
 //const GOOGLE_PLACES_API_KEY = 'AIzaSyDTSx3i96XYjLF2qgnCoR5dh8GwU30N9cY';
 
 const AddStation = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.AdresseEingeben}>
-        <Text style={{fontSize: 26}}>Bitte Adresse eingeben</Text>
-      </View>
-      <View style={styles.autocompleteStyle}>
-        <GooglePlacesAutocomplete
-          GooglePlacesDetailsQuery={{fields: 'geometry'}}
-          fetchDetails={true} // you need this to fetch the details object onPress
-          placeholder="Search"
-          query={{
-            key: 'AIzaSyDTSx3i96XYjLF2qgnCoR5dh8GwU30N9cY',
-            language: 'de', // language of the results
-          }}
-          onPress={(data: any, details: any = null) => {
-            console.log('data', data);
-            console.log('details', details);
-            console.log(JSON.stringify(details?.geometry?.location));
-            let geoData = JSON.stringify(details?.geometry?.location);
-            console.log(geoData);
-          }}
-          onFail={error => console.error(error)}
-        />
-      </View>
-    </SafeAreaView>
+    <View style={styles.autocompleteStyle}>
+      <Text
+        style={{
+          fontSize: 24,
+          width: '80%',
+          marginTop: 100,
+          position: 'relative',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        Bitte Adresse eingeben
+      </Text>
+      <GooglePlacesAutocomplete
+        GooglePlacesDetailsQuery={{fields: 'geometry'}}
+        fetchDetails={true} // you need this to fetch the details object onPress
+        placeholder="Search"
+        query={{
+          key: 'AIzaSyDTSx3i96XYjLF2qgnCoR5dh8GwU30N9cY',
+          language: 'de', // language of the results
+        }}
+        onPress={(data: any, details: any = null) => {
+          console.log('data', data);
+          console.log('details', details);
+          console.log(JSON.stringify(details?.geometry?.location));
+          let geoData = JSON.stringify(details?.geometry?.location);
+          console.log(geoData);
+        }}
+        onFail={error => console.error(error)}
+      />
+      <Button title="Halt bestätigen"></Button>
+    </View>
   );
 };
 export default AddStation;
@@ -38,7 +45,7 @@ const styles = StyleSheet.create({
   autocompleteStyle: {
     flex: 1,
     padding: 40,
-    backgroundColor: '#ff0000',
+    backgroundColor: '#d5d5d5',
   },
   AdresseEingeben: {
     alignItems: 'center',
